624d719e1b7951c75c48a9ea67ab37a38436cab9
eddiec68 <eddiec68@nycstudents.net>
2026-01-28 10:16:42 -0500

    day 2

---
+++ me.py
@@ -0,0 +1,25 @@
+import turtle
+from turtle import *
+t = Turtle()
+
+""" def rectangle(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+rectangle(125) """
+
+
+def equaltriangle(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+equaltriangle(90) 
+
+turtle.done()
